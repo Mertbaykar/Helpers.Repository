5a0e08e70a93307d625d840a70d3f7a2fa8b58ae
Mertbaykar <mertbaykar@hotmail.com>
2022-09-28 22:21:32 +0300

    Add project files.

---
+++ EF.Core.Extensions/DbContextExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EF.Core.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static void BulkUpdate<TEntity>(this DbContext dbcontext, Action<PropertyHelper<TEntity>> propertySettings, Expression<Func<TEntity, bool>> whereExpression) where TEntity : class
+        {
+            try
+            {
+                PropertyHelper<TEntity> propertyHelper = new PropertyHelper<TEntity>(dbcontext);
+                propertyHelper.InitSql();
+                propertySettings.Invoke(propertyHelper);
+
+                // Sql set kısımları üst satırda handle edildi
+                string totalSql = propertyHelper.Sql;
+
+                // Sql'in from kısmı oluştu
+                string sqlFrom = dbcontext.Set<TEntity>().Where(whereExpression).ToQueryString();
+
+                // Query'de 'set' sonrası ilk virgülü kaldır (sql hatası olmaması için)
+                int sqlFirstCommaIndex = totalSql.IndexOf(',');
+                totalSql = totalSql.Remove(sqlFirstCommaIndex, 1);
+
+                // from kısmı query'ye eklendi
+                totalSql += " from " + "(" + sqlFrom + ")";
+
+                propertyHelper.Sql = totalSql;
+                dbcontext.Database.ExecuteSqlRaw(propertyHelper.Sql, propertyHelper.SqlParameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+
+        }
+
+
+        public static void BulkUpdate<TEntity>(this DbContext dbcontext, Action<PropertyHelper<TEntity>> propertySettings) where TEntity : class
+        {
+            try
+            {
+                dbcontext.BulkUpdate(propertySettings, x => 1 == 1);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+    }
+}
---
+++ EF.Core.Extensions/PropertyHelper.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Data.SqlClient;
+
+namespace EF.Core.Extensions
+{
+    public class PropertyHelper<TEntity> where TEntity : class
+    {
+        public PropertyHelper(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+        internal string Sql { get; set; }
+        internal DbContext DbContext { get; set; }
+        internal SqlParameterCollection SqlParameters { get; set; }
+        internal IEntityType EntityType => DbContext.Model.FindEntityType(typeof(TEntity));
+        internal string Schema => EntityType.GetSchema();
+        internal string TableName => EntityType.GetSchemaQualifiedTableName();
+        internal StoreObjectIdentifier StoreObjectIdentifier => StoreObjectIdentifier.Table(TableName, Schema);
+
+        public void SetProperty<TProperty>(Expression<Func<TEntity, TProperty>> propertyExpression, TProperty value) where TProperty : IConvertible
+        {
+            try
+            {
+                if (DbContext is null)
+                    throw new Exception("DbContext is required");
+
+                string propertyName = GetPropertyNameFromExpression(propertyExpression);
+                string columnName = EntityType.FindProperty(propertyName).GetColumnName(StoreObjectIdentifier);
+
+                string sqlParameterKey = "@" + propertyName;
+                if (SqlParameters.Contains(sqlParameterKey))
+                    throw new Exception(propertyName + " property is set more than once");
+
+                // Sql handling
+                SqlParameter sqlParameter = new SqlParameter(sqlParameterKey, value);
+                SqlParameters.Add(sqlParameter);
+                Sql += "," + columnName + "=" + sqlParameterKey;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+
+        }
+
+        internal void InitSql()
+        {
+            Sql = "Update " + TableName + " set ";
+        }
+
+        #region GetPropertyNameFromExpression
+
+        internal string GetPropertyNameFromExpression<TProperty>(Expression<Func<TEntity, TProperty>> expression) where TProperty : IConvertible
+        {
+            try
+            {
+                LambdaExpression lambda = (LambdaExpression)expression;
+                MemberExpression memberExpression;
+
+                if (lambda.Body is UnaryExpression)
+                {
+                    UnaryExpression unaryExpression = (UnaryExpression)(lambda.Body);
+                    memberExpression = (MemberExpression)(unaryExpression.Operand);
+                }
+                else
+                    memberExpression = (MemberExpression)(lambda.Body);
+
+                PropertyInfo property = memberExpression.Member as PropertyInfo;
+                return property.Name;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+        #endregion
+    }
+}

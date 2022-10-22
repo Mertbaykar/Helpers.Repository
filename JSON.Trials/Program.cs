aaf4786960ee8ac4407d240423554c590dfbbc88
Mertbaykar <mertbaykar@hotmail.com>
2022-10-22 21:15:26 +0300

    JsonUtils eklendi, bu class'ta generic method yazılacak

---
+++ JSON.Trials/Program.cs
@@ -0,0 +1,6 @@
+﻿using JSON.Utils;
+using Models;
+
+string filepath = @"D:\Extension.Trials\Models\Company.json";
+var employee = new Employee("Sami", "Çoker", 32);
+JsonUtils.AddEmployeeToCompany(filepath, employee);
